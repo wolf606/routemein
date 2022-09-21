59fd4df86c07572ef1448c1d89e482f797a97bee
Luis Miguel Sanchez Pinilla <luism.sanchezp@autonoma.edu.co>
2022-09-21 19:17:46 -0400

    Add RmiIPv4Lib to handle IPs

--- Library/RmiBinaryNumbersLib.py
+++ Library/RmiBinaryNumbersLib.py
@@ -1,18 +1,16 @@
+import imp
 import string
 
-class binary:
+class binaryNumbersHandler:
     zeros='00000000'
-    digits=['0','1']
-    def __init__(self):
-        self.foo = None
 
     @staticmethod
     def sum(num1: string, num2: string) -> string:
-        return binary.int_to_bin(int(num1,2)+int(num2,2))
+        return binaryNumbersHandler.int_to_bin(int(num1,2)+int(num2,2))
 
     @staticmethod
     def int_to_bin(num: int) -> string:
         res = bin(num)[2:]
         if len(res) < 8:
-            res=binary.zeros[0:8-len(res)]+res
+            res=binaryNumbersHandler.zeros[0:8-len(res)]+res
         return res
--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-from Models.binary import binary as bn
+from Library.RmiBinaryNumbersLib import binaryNumbersHandler as bnh
+from Library.RmiIPv4Lib import IPv4 as ip
 
-print(bn.int_to_bin(255))
+print(bnh.int_to_bin(255))
 
-print(bn.sum('10000', '10000'))
+print(bnh.sum('10000', '10000'))
+
+for i in range(256):
+    print(ip(i, i, i, i))
+
+print(ip(254, 254, 254, 254).get_binary_string())
+one = ip(254, 254, 254, 254)
+one.set_ip_value_binary_str('11111100111111001111110011111100')
+print(one)
